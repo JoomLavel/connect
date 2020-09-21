40d249237d7337ac5cbb50bc0257783e659e8b80
jwalczak <32653379+q-u-o-s-a@users.noreply.github.com>
2020-09-21 20:59:35 +0200

    beginning of JoomLavel/RAD integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,17 +6,14 @@
  * Time: 18:42
  */
 
-require __DIR__ . '/../scr/command/GenericCommand.php';
-require __DIR__ . '/../scr/command/MakeComponent.php';
-require __DIR__ . '/../scr/service/ComponentGenerator.php';
-require __DIR__ . '/../scr/service/ZipGenerator.php';
-
-use App\Command\MakeComponent;
+use JoomLavel\Rad\Command\MakeComponent;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
-$application->add(new MakeComponent($config));
+$MakeComponent = new MakeComponent($config);
+
+$application->add($MakeComponent );
 
 try {
     $application->run();
